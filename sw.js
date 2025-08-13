3e26609df7aee4cad14d0bed3afba7b98cbb3964
Arul1095 <105115504+Arul1095@users.noreply.github.com>
2025-08-13 17:39:44 +0530

    Update v2 sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first shell for your GitHub Pages site under /Suja/
-const CACHE_NAME = "mhm-v1";
+const CACHE_NAME = "mhm-v2";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
